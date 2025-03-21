0948cee5ef0b88789558acacc2937ec722add173
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-03-21 11:14:39 -0300

    theme working, main-table done

---
+++ src/app/components/states/states.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-states',
+  imports: [],
+  templateUrl: './states.component.html',
+  styleUrl: './states.component.scss'
+})
+export class StatesComponent {
+
+}
