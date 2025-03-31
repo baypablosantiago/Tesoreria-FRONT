2c6476b34e1d9f561b9c9cf1b446f6506f10c475
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-03-31 10:30:21 -0300

    policy-create button

--- src/app/components/policy-create/policy-create.component.ts
+++ src/app/components/policy-create/policy-create.component.ts
@@ -1,12 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-policy-create',
-  imports: [MatIconModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule,],
   templateUrl: './policy-create.component.html',
-  styleUrl: './policy-create.component.scss',
-  encapsulation: ViewEncapsulation.Emulated
+  styleUrl: './policy-create.component.scss'
 })
 export class PolicyCreateComponent {
 
