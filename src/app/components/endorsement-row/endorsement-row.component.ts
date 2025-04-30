78635a0a53d63fe65a6f42e2f493442560287514
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-04-30 11:43:02 -0300

    dates with time - amount of endorsements visible

--- src/app/components/endorsement-row/endorsement-row.component.ts
+++ src/app/components/endorsement-row/endorsement-row.component.ts
@@ -7,5 +7,11 @@ import { Component, Input } from '@angular/core';
   styleUrl: './endorsement-row.component.scss'
 })
 export class EndorsementRowComponent {
-  @Input() number!: string; 
+  @Input() number!: string;
+  @Input() amount!: number;
+
+  get endorsementAmount(): string {
+    return this.amount == 0 ? 'Ninguno.' : this.amount.toString();
+  }
 }
+
